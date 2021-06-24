9ee6a60105234df355e254cfcedc9927d5fbaa54
austinandika <austinandika@gmail.com>
2021-06-24 19:05:06 +0700

    add Procfile

--- index.js
+++ index.js
@@ -19,10 +19,6 @@ app.get("/product/:id", (req, res) => {
     }))  
 });
 
-app.listen(5000,() =>{
-    console.log("Server running on port 5000");
-})
-
 app.listen(PORT,() =>{
     console.log("Server running on port" + PORT);
 })
